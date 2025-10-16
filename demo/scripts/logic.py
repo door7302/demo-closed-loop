ff965a8874425c2a406727b0168a3790cb0c0557
droy <droy@juniper.net>
2025-10-16 15:08:23 +0200

    fix

--- demo/scripts/logic.py
+++ demo/scripts/logic.py
@@ -275,7 +275,7 @@ def get_evo_pfe_instance(router_name, fpc_slot, pfe_id):
         ppfe_to_inst = {}
         table_started = False
 
-        for line in op.splitlines():
+        for line in op[1].splitlines():
             line = line.strip()
             if not line:
                 continue
